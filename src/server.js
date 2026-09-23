e3028d2e47b96f90cd631b6aeeb4f5b2ca21cc21
divyesh2123 <divyesh2198@gmail.com>
2026-09-23 21:13:00 +0530

    ddd

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.json({
-        message: "Node.js application is running on Amazon EKS final steps ha",
+        message: "Node.js application is running on Amazon EKS final steps ha final",
         version: "1.0.0",
         environment: process.env.NODE_ENV || "development"
     });
