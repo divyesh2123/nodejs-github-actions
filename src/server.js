1ee0c1418c37b2cba8184150624e36d4bb09a4b5
divyesh2123 <divyesh2198@gmail.com>
2026-09-24 12:59:15 +0530

    this is the example

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.json({
-        message: "Node.js application is running on Amazon EKS final steps ha final",
+        message: "Node.js application is running on Amazon EKS final steps ha final example information ",
         version: "1.0.0",
         environment: process.env.NODE_ENV || "development"
     });
